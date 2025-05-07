ee28ea0c593711c643a97387e132524ba972d28a
LeeSuHyeon <tngus0673@naver.com>
2025-05-07 20:34:14 +0900

    chore: 코드베이스 프로젝트 세팅

---
+++ iTunesAppSuhyun/iTunesAppSuhyun/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  iTunesAppSuhyun
+//
+//  Created by 이수현 on 5/7/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ iTunesAppSuhyun/iTunesAppSuhyun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesAppSuhyun
+//
+//  Created by 이수현 on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
