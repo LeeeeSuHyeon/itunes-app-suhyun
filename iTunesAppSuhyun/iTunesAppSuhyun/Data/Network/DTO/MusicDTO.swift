e351c0a88242767ebd4345501c34dbd2315768d8
LeeSuHyeon <tngus0673@naver.com>
2025-05-08 15:14:46 +0900

    feat: MusicDTO 구현

---
+++ iTunesAppSuhyun/iTunesAppSuhyun/Data/Network/DTO/MusicDTO.swift
@@ -0,0 +1,26 @@
+//
+//  MusicDTO.swift
+//  iTunesAppSuhyun
+//
+//  Created by 이수현 on 5/8/25.
+//
+
+import Foundation
+
+struct MusicDTO: Decodable {
+    let musicId: Int
+    let artist: String
+    let title: String
+    let imageURL: String
+    let releaseDate: String
+    let durationInMillis: Int
+
+    enum CodingKeys: String, CodingKey {
+        case musicId = "trackId"
+        case artist = "artistName"
+        case title = "trackName"
+        case imageURL = "artworkUrl100"
+        case releaseDate
+        case durationInMillis = "trackTimeMillis"
+    }
+}
