7e48ca51d60443d5e94ce49d9b9b2b060ce520cf
LeeSuHyeon <tngus0673@naver.com>
2025-05-08 15:23:48 +0900

    feat: Music Model 구현

---
+++ iTunesAppSuhyun/iTunesAppSuhyun/Domain/Model/Music.swift
@@ -0,0 +1,17 @@
+//
+//  Music.swift
+//  iTunesAppSuhyun
+//
+//  Created by 이수현 on 5/8/25.
+//
+
+import Foundation
+
+struct Music {
+    let musicId: Int
+    let title: String
+    let artist: String
+    let imageURL: String
+    let releaseDate: Date
+    let durationInSeconds: TimeInterval
+}
